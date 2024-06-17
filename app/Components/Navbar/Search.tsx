55ba6d88b74ea26c2e01e14fb81b151d8b2103f2
Arunabho Bhattacharya <barunabho@gmail.com>
2024-06-17 23:22:36 +0530

    navbar UI work started

---
+++ app/Components/Navbar/Search.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+const Search = () => {
+    return (
+        <div
+            className="
+                border-[1px]
+                w-full
+                mid:w-auto
+                py-2
+                rounded-full
+                shadow-sm
+                hover: shadow-md
+                transition
+                cursor-pointer
+                
+            "
+        >
+            <div className="
+             flex
+              flex-row
+               items-center
+                justify-between
+            ">
+                <div >
+
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Search
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className=" text-2xl text-rose-500">
       hello airbnb
     </div>
   );
