dc744850ab290e78a4b11e84c58e2532a6afb5ef
Arunabho Bhattacharya <barunabho@gmail.com>
2024-06-18 23:32:49 +0530

    bug fixed

--- app/Components/Navbar/UserMenu.tsx
+++ app/Components/Navbar/UserMenu.tsx
@@ -18,7 +18,7 @@ const UserMenu = () => {
                 <div onClick={() => { }} className=" hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
                     Airbnb your home
                 </div>
-                <div onClick={() => { toggleOpen }} className=" border-[1px] shadow-sm p-4 md:py-1 md:px-2 border-1-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
+                <div onClick={toggleOpen} className=" border-[1px] shadow-sm p-4 md:py-1 md:px-2 border-1-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
                     <AiOutlineMenu />
                     <div className=" hidden md:block">
                         <Avatar />
