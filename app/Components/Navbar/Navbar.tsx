7bdda50d11a93a0a09f9d43025b20f1b33b3ba25
Arunabho Bhattacharya <barunabho@gmail.com>
2024-06-22 13:32:04 +0530

    logged in Usermenu UI and log out wokring

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -11,9 +11,8 @@ interface NavbarProps {
     currentUser?: User | null;
 }
 const Navbar: React.FC<NavbarProps> = (
-    currentUser
+    { currentUser }
 ) => {
-    console.log({ currentUser });
     return (
         <div className=" fixed w-full bg-white z-10 shadow-sm">
             <div
@@ -23,7 +22,7 @@ const Navbar: React.FC<NavbarProps> = (
                     <div className=" flex flex-row items-center justify-between gap-3 md:gap-0">
                         <Logo/>
                         <Search />
-                        <UserMenu/>
+                        <UserMenu currentUser={currentUser} />
                     </div>
                 </Container>
             </div>
--- app/Components/Navbar/UserMenu.tsx
+++ app/Components/Navbar/UserMenu.tsx
@@ -5,13 +5,19 @@ import Avatar from "../Avatar";
 import { useCallback, useReducer, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
-import LoginModal from "../modals/LoginModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
+interface UserMenuProps {
+    currentUser?: User | null;
+}
 
-const UserMenu = () => {
+const UserMenu: React.FC<UserMenuProps> = (
+    { currentUser }
+) => {
     const registerModal = useRegisterModal();
-    const LoginModal = useLoginModal();
+    const loginModal = useLoginModal();
     const [isOpen, setIsopen] = useState(false);
 
     const toggleOpen = useCallback(() => {
@@ -34,10 +40,25 @@ const UserMenu = () => {
             {isOpen && (
                 <div className=" absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
                     <div className=" flex flex-col cursor-pointer">
-                        <>
-                            <MenuItem onClick={LoginModal.onOpen} label="Login" />
+                        {currentUser ? (
+                            <>
+                            <MenuItem onClick={()=>{}} label="My Trips" />
+                            <MenuItem onClick={()=>{}} label="My Favourites" />
+                            <MenuItem onClick={()=>{}} label="My Reservations" />
+                            <MenuItem onClick={()=>{}} label="My Properties" />
+                            <MenuItem onClick={()=>{}} label="Airbnb my home" />
+                            <hr/>
+                            <MenuItem 
+                            onClick={() => signOut()} 
+                            label="Logout"/>
+                        </>
+                        ) : (
+                            <>
+                            <MenuItem onClick={loginModal.onOpen} label="Login" />
                             <MenuItem onClick={registerModal.onOpen} label="Sign Up"/>
                         </>
+                        )}
+        
                     </div>
                 </div>
             )}
