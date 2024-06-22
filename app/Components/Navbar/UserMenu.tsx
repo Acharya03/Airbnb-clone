802a32154ee4ddf7f619623ee150f87ed8dfb672
Arunabho Bhattacharya <barunabho@gmail.com>
2024-06-22 13:53:33 +0530

    date changed to ISO string

--- app/Components/Navbar/UserMenu.tsx
+++ app/Components/Navbar/UserMenu.tsx
@@ -6,11 +6,11 @@ import { useCallback, useReducer, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
-import { User } from "@prisma/client";
 import { signOut } from "next-auth/react";
+import { safeUser } from "@/app/types";
 
 interface UserMenuProps {
-    currentUser?: User | null;
+    currentUser?: safeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = (
