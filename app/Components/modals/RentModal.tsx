15e5b0488307fa62da8ebeb58dbb4d22ba6eaf69
Arunabho Bhattacharya <barunabho@gmail.com>
2024-06-27 10:23:21 +0530

    map added to Step location

--- app/Components/modals/RentModal.tsx
+++ app/Components/modals/RentModal.tsx
@@ -8,6 +8,8 @@ import { categories } from "../Navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
+
 
 enum STEPS {
     CATEGORY = 0,
@@ -49,6 +51,13 @@ const RentModal = () => {
     const category = watch('category');
     const location = watch('location');
 
+
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false
+    }), [location]);
+
+
+    
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
             shouldValidate: true,
@@ -112,6 +121,7 @@ const RentModal = () => {
                 value={location}
                     onChange={(value) => setCustomValue('location', value)}
                 /> { /* Countries select option */ }
+                <Map center={location?.latlng}/>
             </div>
         )
     }
