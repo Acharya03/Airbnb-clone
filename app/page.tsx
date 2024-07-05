7591157f73ddf2fd197eed0e05fbbe1e1314ad9c
Arunabho Bhattacharya <barunabho@gmail.com>
2024-07-05 10:07:56 +0530

    fetch listings and display

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import ClientOnly from "./Components/ClientOnly";
 import Container from "./Components/Container";
 import EmptyState from "./Components/EmptyState";
+import ListingCard from "./Components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
 
 
-export default function Home() {
-	const isEmpty = true;
-	if(isEmpty) {
+export default async function Home() {
+	const listings = await getListings();
+	const currentUser = getCurrentUser();
+
+
+	if(listings.length == 0) {
 		return (
 			<ClientOnly>
 				<EmptyState showReset/>
@@ -18,9 +24,15 @@ export default function Home() {
 				<div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3
 				lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8"
 				>
-					<div>
-						My future Listings
-					</div>
+					{listings.map((listing: any) => {
+						return (
+							<ListingCard
+								currentUser={currentUser}
+								key={listing.id}
+								data={listing}
+							/>
+						)
+					})}
 				</div>
 			</Container>
 		</ClientOnly>
