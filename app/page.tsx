1d9592a7ba14da913edc746bef3a0840a8e405f3
Arunabho Bhattacharya <barunabho@gmail.com>
2024-07-18 20:35:22 +0530

    trial

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import ClientOnly from "./Components/ClientOnly";
 import Container from "./Components/Container";
 import EmptyState from "./Components/EmptyState";
